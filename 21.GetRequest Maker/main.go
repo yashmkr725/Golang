2dbbc08b2fb67ccae0eec110727cb0527695cfd2
yashmkr725 <yash.mkr725@gmail.com>
2025-07-24 21:34:42 +0530

    this is used to make get request on another server

---
+++ 21.GetRequest Maker/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Get method in Go")
+	PerfromGetRequest()
+}
+
+func PerfromGetRequest() {
+	const myurl = "https://jsonplaceholder.typicode.com/todos/1"
+	res, err := http.Get(myurl)
+	checkError(err)
+
+	defer res.Body.Close() // all things done it is going to close this
+
+	fmt.Println("Status code", res.Status)
+	fmt.Println("Content Length", res.ContentLength)
+
+	var responseString strings.Builder
+	content, err := io.ReadAll(res.Body) // content is in byte format 
+	checkError(err)
+	byteCount ,_:=responseString.Write((content))
+	fmt.Println(byteCount) // 83
+
+	// fmt.Println(string(content)) another way using responseString
+	fmt.Println(responseString.String() ) // better way it uses a library more powerful more features
+
+
+}
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
